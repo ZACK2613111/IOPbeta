e089a7988616e9c4aeb783cf882b7c2d2ba1899e
gasper60ti <kn_fliti@esi.dz>
2022-04-25 19:26:47 +0100

    First push

--- App.js
+++ App.js
@@ -1,27 +1,75 @@
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// Screens
-import Home from "./src/app/screens/Home";
-import Loading from "./src/app/screens/Loading";
-import myPlants from "./src/app/screens/myPlants";
-
-
-//Stack
-
-const Stack = createNativeStackNavigator();
-
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import Weatherbar from './Components/Weatherbar';
+import Donutchart from './Components/Donutchart';
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    'CircularStd-Bold': require('./assets/fonts/CircularStdBold.ttf'),
+    'CircularStd-Book': require('./assets/fonts/CircularStdBook.ttf'),
+    'CircularStd-Medium': require('./assets/fonts/CircularStdMedium.ttf'),
+  });
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ header: () => null}}>
-        {/* <Stack.Screen name="Loading" component={Loading}/>
-        <Stack.Screen name="Home" component={Home}/> */}
-        <Stack.Screen name="myPlants" component={myPlants}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    <View style={styles.dashBoard}>
+      <View style={styles.salut}>
+        <Text style={styles.salutZack}>Salut Zakaria,</Text>
+        <Text style={styles.salutMsg}>N'oubliez pas de passer du temps avec vos plantes...</Text>
+        <Weatherbar style={styles.Weatherbar} />
+        <View style={styles.sectionRond}>
+          <Donutchart />
+        </View>
+      </View>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  dashBoard: {
+    display: 'flex',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  salut: {
+    position: 'absolute',
+    top: 72.5,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    width: 310,
+
+  },
+  salutZack: {
+    position: 'absolute',
+    left: 5,
+    top: 10,
+    fontFamily: 'CircularStd-Bold',
+    fontWeight: '700',
+    fontSize: 24,
+    lineHeight: 27,
+    color: '#07D779'
+  },
+  salutMsg: {
+    position: 'absolute', width: 205,
+    left: 5,
+    top: 37,
+    fontFamily: 'CircularStd-Medium',
+    fontWeight: '700',
+    fontSize: 14,
+    lineHeight: 18,
+    color: '#00000065'
+  },
+  Weatherbar: {
+  },
+  sectionRond: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: 342,
+    top: 195,
+  }
+});
---
+++ Components/Weatherbar.js
@@ -0,0 +1,64 @@
+import { StyleSheet, Image, Text, View } from 'react-native';
+
+
+const Weatherbar = () => {
+  return (
+    <View style={wstyle.wb}>
+
+      <View style={wstyle.wbelem}>
+        <Image source={require('../assets/images/sun.png')} />
+        <Text style={wstyle.wbtxt}>Day</Text>
+      </View>
+
+      <View style={wstyle.wbelem}>
+        <Image source={require('../assets/images/sun.png')} />
+        <Text style={wstyle.wbtxt}>Day</Text>
+      </View>
+      <View style={wstyle.wbelem}>
+        <Image source={require('../assets/images/sun.png')} />
+        <Text style={wstyle.wbtxt}>Day</Text>
+      </View>
+      <View style={wstyle.wbelem}>
+        <Image source={require('../assets/images/sun.png')} />
+        <Text style={wstyle.wbtxt}>Day</Text>
+      </View>
+    </View>
+  )
+}
+const wstyle = StyleSheet.create({
+  wb: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    flexDirection: 'row',
+    position: 'absolute',
+    width: 290,
+    height: 50,
+    top: 107,
+
+    /*INSIDE*/
+    alignSelf: 'stretch',
+    marginVertical: 15,
+  },
+  wbelem: {
+    display: 'flex',
+    width: 29,
+    height: 50,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 30,
+  },
+  wbtxt: {
+    fontFamily: 'CircularStd-Medium',
+    fontSize: 13,
+    lineHeight: 16,
+    textAlign: 'center',
+    color: '#00000090',
+
+
+  }
+})
+
+export default Weatherbar
