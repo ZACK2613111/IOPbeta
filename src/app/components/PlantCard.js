a3173ae4f54ed30c4dc951069cf40e54ff87c744
Hous1423 <kh_talbi@esi.dz>
2022-04-26 10:54:36 +0100

    added zakis screen

---
+++ src/app/components/PlantCard.js
@@ -0,0 +1,36 @@
+import {Image, Pressable, Text, View} from "react-native";
+import COLORS from "../../data/colors";
+
+export default function PlantCard({name, img, etat, deleteHandler, id}) {
+  return (
+    <View
+      style={{
+        width: "48%",
+        borderColor: COLORS.GRAY,
+        borderRadius: 20,
+        borderWidth: 2,
+        marginBottom: 10,
+        marginHorizontal: "1%",
+        overflow: "hidden",
+        // elevation: 10,
+      }}
+    >
+      <Pressable
+        onLongPress={() => deleteHandler(id)}
+        android_ripple={{color: "#000"}}
+        style={{
+          alignItems: "center",
+          backgroundColor: "white",
+          padding: 5,
+        }}
+      >
+        <Image source={img} />
+        <Text style={{color: COLORS.GREEN, fontWeight: "bold", fontSize: 16}}>
+          {name}
+        </Text>
+        <Text style={{fontWeight: "bold", color: "black"}}>Jasmine</Text>
+        <Text style={{color: COLORS.GRAY, fontWeight: "600"}}>{etat}</Text>
+      </Pressable>
+    </View>
+  );
+}
--- src/app/screens/myPlants.js
+++ src/app/screens/myPlants.js
@@ -1,176 +1,157 @@
-import {View, StyleSheet, Image, Text, ScrollView, TouchableOpacity} from "react-native";
-import React from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  FlatList,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Icon,
+  TextInput,
+  Pressable,
+} from "react-native";
 
 
+
+import data from "../../data/plants";
+import COLORS from "../../data/colors"
+import {useState} from "react";
+import PlantCard from "../components/PlantCard";
 
 export default function MyPlants() {
+  const [value, setValue] = useState("");
+  const [plants, setPlants] = useState(data);
+  const Example = () => {
+    return (
+      <Center>
+        <Box
+          height="200"
+          w="400"
+          shadow="2"
+          rounded="lg"
+          _dark={{
+            bg: "coolGray.200:alpha.20",
+          }}
+          _light={{
+            bg: "coolGray.200:alpha.20",
+          }}
+        >
+          <Fab
+            renderInPortal={false}
+            shadow={2}
+            placement="top-right"
+            size="sm"
+            icon={
+              <Icon
+                color="white"
+                as={MaterialIcons}
+                name="lightbulb"
+                size="4"
+              />
+            }
+            label="Quick Start"
+          />
+        </Box>
+      </Center>
+    );
+  };
+  const sortedFilteredPlants = () => {
+    let sortedPlant = plants;
+    sortedPlant.sort((a, b) =>
+      b.name.toLocaleLowerCase() > a.name.toLocaleLowerCase() ? -1 : 1
+    );
+    sortedPlant = sortedPlant.filter((item) =>
+      item.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    );
+    return sortedPlant;
+  };
+  const deleteHandler = (id) => {
+    const index = sortedFilteredPlants().findIndex((item) => item.id === id);
+    const newData = [...plants];
+    newData.splice(index, 1);
+    setPlants(newData);
+  };
   return (
-    // Header
-    // Serach bar
-
-    <View>
-      <Text>myPlants</Text>
-
-      <ScrollView contentContainerStyle={styles.scroll}>
-        <View >
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
+    <View
+      style={{alignItems: "center", backgroundColor: "white", height: "100%"}}
+    >
+      <View
+        style={{
+          height: (10 * Dimensions.get("screen").height) / 100,
+          backgroundColor: "red",
+          width: "100%",
+        }}
+      ></View>
+      <View
+        style={{
+          height: (75 * Dimensions.get("screen").height) / 100,
+          width: "80%",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            marginTop: 10,
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              borderColor: "#00000025",
+              borderWidth: 2,
+              borderRadius: 20,
+              flex: 1,
+              marginRight: 5,
             }}
-            delayLongPress={3000}
           >
             <Image
-              style={{}}
-              source={require="../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
+              source={require("../../assets/myPlants/search.png")}
+              style={{height: 40, width: 40}}
+              resizeMode={"contain"}
             />
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
+            <TextInput
+              placeholder="Search"
+              value={value}
+              onChangeText={setValue}
+              style={{flex: 1}}
+            />
+          </View>
+          <Pressable
+            android_ripple={{
+              color: "#00000025",
             }}
-            delayLongPress={3000}
           >
-           <Image source = {require='../../assets/myPlants/plants/plant1.png'}></Image>          
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
+            <Image source={require("../../assets/myPlants/sliders.png")} />
+          </Pressable>
         </View>
-
-        <View>
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
-            }}
-            delayLongPress={3000}
-          >
-            {/* <Image
-              style={{}}
-              source={require="../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
-            /> */}
-           <Image source = {require='../../assets/myPlants/plants/plant1.png'}></Image>          
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
-            }}
-            delayLongPress={3000}
-          >
-            <Image
-              style={{}}
-              source={require="../../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
-            />
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
+        <View style={{marginTop: 10, flex: 1, alignItems: "center"}}>
+          <FlatList
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+            data={sortedFilteredPlants()}
+            renderItem={({item, index}) => (
+              <PlantCard
+                name={item.name}
+                img={item.img}
+                etat={"Bon état"}
+                deleteHandler={deleteHandler}
+                id={item.id}
+              />
+            )}
+          />
         </View>
-
-        <View>
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
-            }}
-            delayLongPress={3000}
-          >
-            <Image
-              style={{}}
-              source={require="../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
-            />
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
-            }}
-            delayLongPress={3000}
-          >
-            <Image
-              style={{}}
-              source={require="../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
-            />
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View>
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
-            }}
-            delayLongPress={3000}
-          >
-            <Image
-              style={{}}
-              source={require="../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
-            />
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={[styles.plantContainer]}
-            onLongPress={() => {
-              console.log("Selected");
-            }}
-            delayLongPress={3000}
-          >
-            <Image
-              style={{}}
-              source={require="../assets/myPlants/plants/plant1.png"}
-              resizeMode="contain"
-            />
-            <Text>Lorem Ipsum</Text>
-            <Text>Jasmine</Text>
-            <Text>Bon état</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+      </View>
+      <View
+        style={{
+          height: (15 * Dimensions.get("screen").height) / 100,
+          backgroundColor: "green",
+          width: "100%",
+        }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    justifyContent: "center",
-    alignContent: "center",
-    marginTop: 60,
-    marginRight: 30,
-    marginLeft: 30,
-  },
-  plantContainer: {
-    justifyContent: "center",
-    alignContent: "center",
-    margin: 5,
-    marginBottom: 55,
-    borderRadius: 40,
-  },
-  scroll:{justifyContent:'center', alignContent:'center'},
-});
